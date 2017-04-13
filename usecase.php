2187797e1bc57339ab81977c474789327e69eb27
S JACKSON <jackson.sabastian@gmail.com>
2017-04-13 10:44:03 -0600

    use case complete

--- usecase.php
+++ usecase.php
@@ -1,7 +1,19 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: AmbientCreativiT
- * Date: 4/13/2017
- * Time: 10:13
- */
+<!Doctype html>
+<meta charset="utf=8" xmlns="http://www.w3.org/1999/html">
+<html>
+	<head>
+		<title>U S E x C A S E</title>
+	</head>
+	<br>
+		<p>Michael Jordan is browsing sandals and is favoriting (marking) his last favored sandal.</p>
+		<br>
+		<p><i>Basic Flows</i> </p>
+	<br>
+		<ul>
+			<b>MJ</b> logs in - Searches for Produce - Site Returns Query - Mike Finds what he Likes - Favorites</ul>
+		<ul>
+		<b>MJ</b> logs in - Searches Random Produce - Site Returns Random Results - Finds Product - Mike Favorites
+		</ul>
+
+	</body>
+</html>
