0e254cce693c500ac40a7fd03f70153cc77d8d6c
S JACKSON <jackson.sabastian@gmail.com>
2017-04-14 06:50:51 -0600

    updated persona

--- persona.php
+++ persona.php
@@ -4,17 +4,23 @@
 		<title>
 			Persona
 		</title>
-		<p>
-			Jacko - 30 - Gamer
-		</p>
-			<h1></h1>
-			Jacko is a professional gamer -
-		Chromebox user - Connects to a 4K Projected Display - Latest Flagship Google Phone. Using phone to quickly browse Etsy. Jacko is making a last minute purchase would like to get done before end of business day. Has found 2/3 options to throughout the day.
-
-		Must be able to see his favorited products.
-		<br>
-		Pressed for time
-		</p>
-
 	</head>
+		<body>
+		<h1>User</h1>
+		<p>
+			Jacko - 23 - Software Developer
+		</p>
+			<h1>Accessibility</h1>
+		Chromebox user - Connects to a 4K Projected Display - Latest Flagship Google Phone. Using phone to quickly browse Etsy.
+		<h1>Frustrations & Needs</h1>
+		Jacko is making a last minute purchase would like to get done before end of business day. Has found 2/3 options to throughout the day. Must be able to see his favorited products. Pressed for time.
+		<h1>Abilities & Knowledge</h1>
+		<p>
+			Fibered internet connection at home, 4G to 3G internet connection on the go, can connect to public wifi, well rounded computer knowledge
+		</p>
+			<h1>User Goal</h1>
+			<p>
+				Find three options for sandals to buy for house guests before end of business day
+			</p>
+		</body>
 </html>
--- usecase.php
+++ usecase.php
@@ -5,9 +5,9 @@
 		<title>U S E x C A S E</title>
 	</head>
 	<br>
-		<p>Michael Jordan is browsing sandals and is favoriting (marking) his last favored sandal around 3:30p! A Chromebox user - Connects to a 4K Projected Display. His latest Flagship Google Phone could be used at any moment. His friends are coming over and he wants to make sure that they are comfortable. He's tried many other eCommerce websites but none seemed to have his desired taste in sandals like BadEtsy has</p>
+		<p></p>
 		<p><i>Basic Flows</i></p>
-			<p><b>MJ</b> logs in - Searches for Produce - Site Returns Query - Mike Finds what he Likes - Favorites</p>
+			<p><b>Jacko</b> logs in - Searches for Produce - Site Returns Query - Mike Finds what he Likes - Favorites</p>
 			<p><b>MJ</b> logs in - Searches Random Produce - Site Returns Random Results - Finds Product - Mike Favorites</p>
 
 	</body>
